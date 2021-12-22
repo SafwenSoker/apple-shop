a93070f5777b4781d7ba0d030eaa69ca72836445
safwensoker <safwensoker1@gmail.com>
2021-12-23 00:49:23 +0100

    Generate 404 component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,19 @@ import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 const routes: Routes = [
+  { path : "home", component : HomeComponent},
   { path : "about", component : AboutComponent},
   { path : "contact", component : ContactComponent},
-  { path : "", component : HomeComponent}
+  { path : "**", component : NotFoundComponent}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  declarations: [
+    NotFoundComponent
+  ]
 })
 export class AppRoutingModule { }
