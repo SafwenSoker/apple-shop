a89b0d2621157896b72eb359ef24e93f4a74c5df
safwensoker <safwensoker1@gmail.com>
2021-12-30 11:40:10 +0100

    Get courses list from file: courses-list

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+import { courses } from 'src/app/courses-list';
+import { Course } from 'src/app/course.model';
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.css']
 })
 export class CoursesComponent implements OnInit {
+
+  public coursesList: Course[] = courses;
 
   constructor() { }
 
