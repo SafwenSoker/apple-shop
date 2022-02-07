91f4b30239b3aaa11b169139cd7f256615cd23e2
SafwenSoker <safwensoker1@gmail.com>
2022-02-07 22:53:51 +0100

    display course details

--- src/app/courses/courses-routing.module.ts
+++ src/app/courses/courses-routing.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CoursesComponent } from './courses.component';
 import { CourseDetailsComponent } from './course-details/course-details.component';
-
+import { CommonModule } from '@angular/common';
 const routes: Routes = [
   { path: '', component: CoursesComponent },
   { path: 'course-details', component: CourseDetailsComponent },
   { path: ':id', component: CourseDetailsComponent}
 ];
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [RouterModule.forChild(routes), CommonModule],
   exports: [RouterModule],
   declarations: [
     CourseDetailsComponent
