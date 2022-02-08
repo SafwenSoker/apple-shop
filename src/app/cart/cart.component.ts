01c3a453d3ac201eff98059e98747da57b504209
SafwenSoker <safwensoker1@gmail.com>
2022-02-09 00:52:52 +0100

    compute and display total price

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from 'src/app/services/cart.service';
+import { courses } from 'src/app/courses-list';
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
@@ -8,12 +9,29 @@ import { CartService } from 'src/app/services/cart.service';
 export class CartComponent implements OnInit {
 
   public cartContent: any = [];
+  totalPrice: number = 0;
+  TAX_RATE: number = 0.2;
   constructor(private cartService: CartService) { }
 
   ngOnInit(): void {
-    this.cartContent = this.cartService.cartContent;
-    console.log(this.cartContent);
-    
+    this.getCartContentDetails();
+    this.computeTotalPrice();    
   }
 
+  getCartContentDetails() {
+    this.cartContent = this.cartService.cartContent;
+    for (let index = 0; index < this.cartContent.length; index++){
+      const course = courses.filter(course => course.id == this.cartContent[index].id)[0];
+      this.cartContent[index].title = course.title;
+      this.cartContent[index].price = course.price;
+    }
+  }
+
+  computeTotalPrice() {
+    let totalPrice = 0;
+    this.cartContent.forEach((item: { price: number; quantity: number; }) => {
+      totalPrice += item.price * item.quantity;
+    });
+    this.totalPrice = totalPrice;
+  }
 }
