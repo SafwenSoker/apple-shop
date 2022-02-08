dfb17a6d130af7e85d5e4d60fc1f315a6a1cfe9b
SafwenSoker <safwensoker1@gmail.com>
2022-02-08 01:27:13 +0100

    Refactoring : use cart service in courses component

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { courses } from 'src/app/courses-list';
 import { Course } from 'src/app/course.model';
-import { LocalStorageService } from 'src/app/services/local-storage.service'
+import { CartService } from 'src/app/services/cart.service';
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
@@ -11,16 +11,12 @@ export class CoursesComponent implements OnInit {
 
   public coursesList: Course[] = courses;
 
-  public cartContent: any[]= [];
-
-  constructor(private localStorageService: LocalStorageService) {}
+  constructor(private cartService: CartService) {}
 
   ngOnInit(): void {
-    this.cartContent = this.localStorageService.get('cart');
   }
 
-  public addToCart(id: String):void {
-    this.cartContent.filter(elem => elem.id === id)[0] ? this.cartContent.filter(elem => elem.id === id)[0].quantity++ : this.cartContent.push({id: id, quatity: 1});
-    this.localStorageService.set('cart', this.cartContent);
+  public addToCart(id?: string):void {
+    this.cartService.add(id);
   }
 }
