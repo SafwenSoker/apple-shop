63a8c3c57122b26c184652b180e7e71704b6f63c
SafwenSoker <safwensoker1@gmail.com>
2022-02-09 22:19:21 +0100

    Create auth middleware

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
+const auth = require('./../middlewares/auth')
 const userController = require('./../controllers/user')
 
 router.post('/signup', userController.signup)
 router.post('/login', userController.login)
-router.get('/profile/:id', userController.profile)
+router.get('/profile/:id', auth, userController.profile)
 module.exports = {
     login, signup
 }
