c9eb9faeae4dddae06ea13a2f7d78082933a2141
SafwenSoker <safwensoker1@gmail.com>
2022-02-09 19:10:55 +0100

    signup api

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,6 +1,24 @@
-signup = (req,res,next)=> {}
+const User = require('./../models/user')
+const bcrypt = requrie('bcrypt')
 
-login = (req,res,next) => {}
+signup = (req,res,next)=> {
+    bcrypt.hash(req.body.password, 10)
+    .then(hash => {
+        const user = new User({
+            name: req.body.name,
+            email: req.body.email,
+            password: hash
+        });
+        user.save()
+        .then(() => res.statuss(200).json({message: 'User created ! '}))
+        .catch(error => res.status(400).json({error}));
+    })
+    .catch(error => res.status(500).json({error}))
+}
+
+login = (req,res,next) => {
+    
+}
 
 module.exports = {
     signup,
