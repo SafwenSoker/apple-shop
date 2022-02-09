6a2aae7c1608986749e0e1c9c16a8c613467a52a
SafwenSoker <safwensoker1@gmail.com>
2022-02-09 23:03:08 +0100

    Define authService signup method

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
 
+import { HttpHeaders, HttpClient, HttpErrorResponse } from '@angular/common/http';
+
+import { Observable, throwError, catchError } from 'rxjs';
+
+import { User } from 'src/app/user.model';
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  API_URL: String = 'http://localhost:3000/api';
+  headers = new HttpHeaders().set('Content-Type', 'application-json');
+
+  constructor(private http: HttpClient) { }
+
+  signup(user: User): Observable<any>{
+    return this.http.post(`${this.API_URL}/auth/signup`, user).pipe(
+      catchError(this.handleError)
+    )
+  }
+  
+  handleError(error: HttpErrorResponse) {
+    let msg=''
+    if(error.error instanceof ErrorEvent) {
+      msg = error.error.message;
+    } else {
+      msg = `Error code: ${error.status}\nMessage: ${error.message}`;
+    }
+    return throwError(msg);
+  }
+
 }
