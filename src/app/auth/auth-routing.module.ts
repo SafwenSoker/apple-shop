5b7564facdc8c79cee5e28a6c850f1e9c086b239
SafwenSoker <safwensoker1@gmail.com>
2022-02-09 22:28:41 +0100

    Create signup component

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './auth.component';
 import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [{ path: '', component: AuthComponent }];
 
@@ -9,7 +10,8 @@ const routes: Routes = [{ path: '', component: AuthComponent }];
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
   declarations: [
-    LoginComponent
+    LoginComponent,
+    SignupComponent
   ]
 })
 export class AuthRoutingModule { }
